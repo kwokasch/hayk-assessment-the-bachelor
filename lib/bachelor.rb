2ce1f28e0f7d546d7e1e340ff6fd13102de65809
github username <github email address>
2019-08-28 16:59:33 -0600

    completed

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,55 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  contestant = data[season].filter do |contestant| 
+    contestant["status"] == "Winner"  
+  end
+  contestant.first["name"].split(" ").first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestant| 
+    contestant.each do |contestant_info| 
+      if contestant_info["occupation"] == occupation
+        return contestant_info["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  town_count = 0
+  data.each do |season, contestant|
+    contestant.each do |contestant_info|
+      if contestant_info["hometown"] == hometown
+        town_count +=1
+      end
+    end    
+  end
+  town_count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestant| 
+    contestant.each do |contestant_info| 
+      if contestant_info["hometown"] == hometown
+        return contestant_info["occupation"]
+      end
+    end
+  end
 end
 
+# def get_occupation(data, hometown)
+#   data.each do |season, contestant| 
+#     contestant.select do |contestant_info| 
+#       contestant_info["hometown"] == hometown
+#     end.map do |new_contestant|
+#     new_contestant["occupation"]
+#   end
+# end
+
 def get_average_age_for_season(data, season)
-  # code here
+  (data[season].map do |contestant|
+    contestant["age"].to_i
+  end.reduce(:+).to_f / data[season].length).round
 end
